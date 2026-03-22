75999dea8288592e4cc7ea6dd5b6255ca79a9ecd
Youness Ben <tudorbenhadda@gmail.com>
2026-03-22 23:48:28 +0100

    fixed minor problems

--- NativeType.h
+++ NativeType.h
@@ -9,25 +9,25 @@ typedef NTSTATUS (NTAPI* pNtOpenProcess)
 (
     PHANDLE ProcessHandle,
     ACCESS_MASK DesiredAccess,
-    POBJECT_ATTRIBUTES objectAttributes,
-    PCLIENT_ID clientId
+    POBJECT_ATTRIBUTES ObjectAttributes,
+    PCLIENT_ID ClientId
 );
 
 // I want to explain in but just go read to documentation :p
 
 // NtClose
 
-typedef NTSTATUS (NTAPI* pNTClose)
+typedef NTSTATUS (NTAPI* pNtClose)
 (
-    HANDLE handle
+    HANDLE Handle
 );
 
 // NtQuerySystemInformation
 
 typedef NTSTATUS(NTAPI* pNtQuerySystemInformation)
 (
-    SYSTEM_INFORMATION_CLASS systemInformationClass,
-    PVOID SystemInformation,
-    ULONG SystemInformationLength,
-    PULONG ReturnLenght
+    SYSTEM_INFORMATION_CLASS systemInformationClass, // It specify what information were going to retrieve
+    PVOID SystemInformation, // defining a pointer to use 
+    ULONG SystemInformationLength, // just the size of the buffer
+    PULONG ReturnLength // this is a parameter that get the actual size of the data returned 
 );
