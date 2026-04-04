2ca5f6e5c8d62a27858aeba8c2b5d07e34c35ec4
Youness Ben <tudorbenhadda@gmail.com>
2026-04-04 12:32:39 +0100

    Working on Hooks

--- Hook.cpp
+++ Hook.cpp
@@ -1,2 +1,61 @@
 #include "MinHook.h"
+#include "NativeType.h"
 
+pNtAllocateVirtualMemory OriginalNtAllocateVirtualMemory = nullptr;
+
+NTSTATUS NTAPI HookNtAllocateVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+    ULONG AllocationType,
+    ULONG Protect
+);
+
+
+pNtWriteVirtualMemory OriginalNtWriteVirtualMemory = nullptr;
+
+NTSTATUS NTAPI HookNtWriteVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID BaseAddress,
+    PVOID Buffer,
+    SIZE_T NumberOfBytesToWrite,
+    PSIZE_T NumberOfBytesWritten
+);
+
+pNtReadVirtualMemory OriginalNtReadVirtualMemory = nullptr;
+
+NTSTATUS NTAPI HookNtReadVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID BaseAddress,
+    PVOID Buffer,
+    SIZE_T NumberOfBytesToRead,
+    PSIZE_T NumberOfBytesRead
+);
+
+pNtAllocateVirtualMemory OriginalNtAllocateVirtualMemory = nullptr;
+
+NTSTATUS NTAPI HookNtAllocateVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+    ULONG AllocationType,
+    ULONG Protect
+);
+
+pNtCreateThreadEx OriginalNtCreateThreadEx = nullptr;
+
+NTSTATUS NTAPI HookNtCreateThreadEx(
+    PHANDLE ThreadHandle,
+    ACCESS_MASK DesiredAccess,
+    POBJECT_ATTRIBUTES ObjectAttributes,
+    HANDLE ProcessHandle,
+    PVOID StartAddress,
+    PVOID Parameter,
+    ULONG CreateFlags,
+    SIZE_T ZeroBits,
+    SIZE_T StackSize,
+    SIZE_T MaximumStackSize,
+    PVOID AttributeList
+);
